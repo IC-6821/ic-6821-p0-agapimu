3e8f4f3fa897a3078411a8e684ccb66dcb1ed11d
DaniloDuque <dduque@estudiantec.cr>
2024-08-19 22:09:17 -0600

    EasyAI formated

--- app/src/main/java/org/classes/EasyAI.java
+++ app/src/main/java/org/classes/EasyAI.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class EasyAI implements MoveMaker {
+public final class EasyAI implements MoveMaker {
 
     public Coordinate placeToken(Board board) {
-        List<Coordinate> freePoints = new ArrayList<>();
-        for (int i = 0; board.isBoundRow(i); ++i)
+        final List<Coordinate> freePoints = new ArrayList<>();
+        for (int i = 0; board.isBoundRow(i); ++i) {
             for (int j = 0; board.isBoundColumn(j); ++j) {
-                Coordinate coordinate = new Coordinate(Row.values()[i], Column.values()[j]);
-                if (board.isFree(coordinate)) freePoints.add(coordinate);
+                final Coordinate coordinate = new Coordinate(Row.values()[i], Column.values()[j]);
+                if (board.isFree(coordinate)){
+                    freePoints.add(coordinate);
+                }
             }
-        if (freePoints.isEmpty()) return null;
+        }if (freePoints.isEmpty()){
+            return null;
+        }
         Collections.shuffle(freePoints);
         return freePoints.getFirst();
     }
