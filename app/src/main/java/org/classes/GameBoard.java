a8f20f492d528474d4a38546a8caff48316c1adb
Luco1421 <bykuco1421@gmail.com>
2024-08-19 20:58:10 -0600

    isFree

--- app/src/main/java/org/classes/GameBoard.java
+++ app/src/main/java/org/classes/GameBoard.java
@@ -2,13 +2,18 @@ package org.classes;
 
 public interface GameBoard {
     void show(View view);
+
     GameState isGameOver();
 
     boolean isBoundRow(int x);
 
     boolean isBoundColumn(int x);
+
     void placeTokenX(Coordinate coordinate, View view);
+
     void placeTokenO(Coordinate coordinate, View view);
+
+    boolean isFree(Coordinate coordinate);
 
     boolean isX(Coordinate coordinate);
 
