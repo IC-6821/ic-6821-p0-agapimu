5ffd997f08f84dd12394522ae03c0724bcbd3405
Luco1421 <bykuco1421@gmail.com>
2024-08-19 21:01:53 -0600

    enum_bug

--- app/src/main/java/org/classes/EasyAI.java
+++ app/src/main/java/org/classes/EasyAI.java
@@ -10,7 +10,7 @@ public class EasyAI implements MoveMaker {
         List<Coordinate> freePoints = new ArrayList<>();
         for (int i = 0; board.isBoundRow(i); ++i)
             for (int j = 0; board.isBoundColumn(j); ++j) {
-                Coordinate coordinate = new Coordinate(i, j);
+                Coordinate coordinate = new Coordinate(Row.values()[i], Column.values()[j]);
                 if (board.isFree(coordinate)) freePoints.add(coordinate);
             }
         if (freePoints.isEmpty()) return null;
